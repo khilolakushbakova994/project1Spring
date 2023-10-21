cd90bd5e7e6396477e82e73409fc4e53b4d612c9
khilola.kushbakova <xilola_kushbakova@mai.ru>
2023-10-21 17:38:33 +0300

    ch

--- src/main/java/com/itproject/projectsample/WelcomingPageController.java
+++ src/main/java/com/itproject/projectsample/WelcomingPageController.java
@@ -44,6 +44,5 @@ public class WelcomingPageController {
     public String divide(@RequestParam("num1") int number1, @RequestParam("num2") int number2) {
         return calculatorService.divide(number1, number2);
 
-
     }
 }
